51cac981423ff0b1bb89924efdd622c66bb5519c
Ragul8775 <101054801+Ragul8775@users.noreply.github.com>
2023-04-18 01:34:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ let movies = [
       name: "falcon and the winter soldier",
       des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam."
         ,
-      image: "hotstar-main/images/slider 2.png"
+      image: "hotstar-main/images/slider 2.PNG"
     },
     {
       name: "loki",
       des:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
-      image: "hotstar-main/images/slider 1.png"
+      image: "hotstar-main/images/slider 1.PNG"
     },
     {
       name: "wanda vision",
       des:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
-      image: "hotstar-main/images/slider 3.png"
+      image: "hotstar-main/images/slider 3.PNG"
     },
     {
       name: "raya and the last dragon",
       des:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
-      image: "hotstar-main/images/slider 4.png"
+      image: "hotstar-main/images/slider 4.PNG"
     },
     {
       name: "luca",
       des:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus officia adipisci autem mollitia quidem totam.",
-      image: "hotstar-main/images/slider 5.png"
+      image: "hotstar-main/images/slider 5.PNG"
     }
   ];
   const carousel = document.querySelector(".carousel");
